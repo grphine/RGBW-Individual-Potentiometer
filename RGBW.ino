c872152d7f4e71b385c81278a0236095a6ae5fa3
grphine <grphine@gmail.com>
2018-06-25 21:37:27 +0100

    Removed testing code

--- RGBW.ino
+++ RGBW.ino
@@ -8,7 +8,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, PIN, NEO_RGBW + NEO_KHZ800);
 int pRed = A0;
 int pGreen = A1;
 int pBlue = A2;
-//int pWhite = A3;
+int pWhite = A3;
 int readR;  // Use this variable to read Potentiometer
 int readG;
 int readB;
@@ -34,20 +34,16 @@ void loop() {
  readR = analogRead(pRed) / 4;
  readG = analogRead(pGreen) / 4;
  readB = analogRead(pBlue) / 4;
- //readW = analogRead((255./1023.) * pWhite);
+ readW = analogRead(pWhite) / 4;
 
- //setAll(readR, readG, readB, readW);
- setAllt(readR, readG, readB);
+ setAll(readR, readG, readB, readW);
 
  Serial.println("---------------------------------");
  Serial.println(readR);
- //Serial.println(pRed);
- //Serial.println(255./1023.);
  Serial.println(readG);
  Serial.println(readB);
- //Serial.println(analogRead(pWhite));
+ Serial.println(readW);
 
-// delay(500);
 }
 
 int setAll(int R, int G, int B, int W){
@@ -59,13 +55,5 @@ int setAll(int R, int G, int B, int W){
   strip.show();
 }
 
-int setAllt(int R, int G, int B){
-  int i = 0;
-   for(i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, G, R, B);
-    
-  }
-  strip.show();
-}
 
 
